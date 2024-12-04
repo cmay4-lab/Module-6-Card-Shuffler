afe28f921ac931f6cc497b9fe22d519a3663e210
cmay4-lab <cmay4@bucs.fsw.edu>
2024-12-04 00:09:34 -0500

    Big Improvement, everything is working now.

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,113 @@
 public class Card {
-    
+    private char rank;    // The card's rank from ace to king
+    private char suit;    // The card's suit (clubs, diamonds, hearts, spades)
+
+    public Card() {
+        rank = '0';
+        suit = '0';
+    }
+
+    public Card(char cardRank, char cardSuit) {
+        rank = cardRank;
+        suit = cardSuit;
+    }
+
+    public char getRank() {
+        return rank;
+    }
+
+    public char getSuit() {
+        return suit;
+    }
+
+    private static int getIntegerAttributeValues(char cardField) {
+        int value = 0;
+
+        switch (cardField) {
+            /* Suits */
+            case 'C' -> value = 1;      // Clubs
+            case 'D' -> value = 2;      // Diamonds
+            case 'H' -> value = 3;      // Hearts
+            case 'S' -> value = 4;      // Spades
+            /* Ranks */
+            case 'A' -> value = 1;      // 1
+            case '2' -> value = 2;      // 2
+            case '3' -> value = 3;      // 3
+            case '4' -> value = 4;      // 4
+            case '5' -> value = 5;      // 5
+            case '6' -> value = 6;      // 6
+            case '7' -> value = 7;      // 7
+            case '8' -> value = 8;      // 8
+            case '9' -> value = 9;      // 9
+            case 'T' -> value = 10;     // 10
+            case 'J' -> value = 11;     // Jack
+            case 'Q' -> value = 12;     // Queen
+            case 'K' -> value = 13;     // King
+        }
+
+        return value;
+    }
+
+    public static Card compare(Card firstCard, Card secondCard) {
+        Card betterCard;                    // The card that is more "valuable"
+        int[] rankValues = new int[2];      // The values used to determine the "correct" order of the ranks (as determined by me)
+        int[] suitValues = new int[2];      // The values used to determine the "correct" order of the suits (as determined by me)
+
+        /* There are 13 ranks (Ace, 2, 3, ..., 10, Jack, Queen, and King) */
+        rankValues[0] = getIntegerAttributeValues(firstCard.rank);
+        rankValues[1] = getIntegerAttributeValues(secondCard.rank);
+
+        /* There are 4 suits (Clubs, Hearts, Spades, and Diamonds) */
+        suitValues[0] = getIntegerAttributeValues(firstCard.suit);
+        suitValues[1] = getIntegerAttributeValues(secondCard.suit);      
+
+        boolean firstCardIsValid = rankValues[0] != 0 && suitValues[0] != 0;
+        boolean secondCardIsValid = suitValues[0] != 0 && suitValues[1] != 0;
+
+        /* Validates the two cards in the first level of scope. Compares the value of the suits in the second scope. And if the suits are equal, 
+        compares the value of the ranks in the final scope. */
+        if (!secondCardIsValid) {
+
+            betterCard = firstCard;
+
+        } else if (!firstCardIsValid) {
+            
+            betterCard = secondCard;
+
+        } else {
+
+            if (suitValues[0] > suitValues[1]) {
+
+                betterCard = firstCard;
+
+            } else if (suitValues[0] < suitValues[1]) {
+
+                betterCard = secondCard;
+
+            } else {
+
+                if (rankValues[0] >= rankValues[1]) {
+
+                    betterCard = firstCard;
+
+                } else {
+
+                    betterCard = secondCard;
+
+                }
+
+            }
+
+        }
+
+        return betterCard;
+
+    }
+
+    public String toStringCard() {
+        char[] cardCharacters = {rank, suit};
+        String cardString = new String(cardCharacters);
+        return cardString;
+    }
+
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,28 +1,147 @@
+import java.util.Random;
+
 public class Deck {
-    private Card[] cards;   // Array of cards
+    
+
+    private static final int STANDARD_DECK_SIZE = 52;       // Hardcoded to be a mutiple of 13
+    private Card[] cards;                                   // Array of cards
 
     /**
      * Creates empty deck.
      * 
      */
     public Deck() {
-        cards = new Card[52];
+        cards = new Card[STANDARD_DECK_SIZE];
     }
 
     public void fill() {
 
+        char[] orderedRanks = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'};
+
+        for (int i = 0; i < STANDARD_DECK_SIZE; i += 13) {
+
+            char suit;
+
+            switch (i) {
+                case 0  -> suit = 'C';
+                case 13 -> suit = 'D';
+                case 26 -> suit = 'H';
+                case 39 -> suit = 'S';
+                default -> suit = '0';
+            }
+
+            for (int j = i; j < i + 13; j++) {
+
+                char rank = orderedRanks[j - i];
+                cards[j] = new Card(rank, suit);
+
+            }
+
+        }
+        
 
     }
 
     public void shuffle() {
 
+        Card[] tempCards = new Card[STANDARD_DECK_SIZE];
 
+        for (int i = 0; i < STANDARD_DECK_SIZE; i++) {
+            tempCards[i] = cards[i];
+        }
 
+        Random randomNumGen = new Random();
+        int startingIndex = randomNumGen.nextInt(STANDARD_DECK_SIZE);   // Between 0 and 51 (Indexes)
+        int[] completedIndexes = new int[52];
+        int randomIndex;
+
+        for (int i = startingIndex; i < STANDARD_DECK_SIZE; i++) {
+
+            randomIndex = randomNumGen.nextInt(STANDARD_DECK_SIZE);
+
+            if (completedIndexes[randomIndex] == randomIndex) {
+
+                int tempRandomIndex;
+                int tempCount = 1;
+
+                do { 
+                    tempRandomIndex = randomNumGen.nextInt(STANDARD_DECK_SIZE);
+
+                    if (randomIndex < 51) {
+                        randomIndex++;
+                    } else {
+                        randomIndex = 0;
+                    }
+
+                    tempCount++;
+
+                } while (completedIndexes[randomIndex] == randomIndex && tempCount < 52);
+
+                if (tempCount >= 52) {
+                    randomIndex = -1;
+                } else {
+                    randomIndex = tempRandomIndex;
+                }
+
+            }
+
+            if (randomIndex > -1) {
+
+                completedIndexes[randomIndex] = randomIndex;
+                cards[randomIndex]  = tempCards[i];
+
+            }
+
+        }
+
+        if (startingIndex != 0) {
+
+            for (int i = 0; i < startingIndex; i++) {
+
+                randomIndex = randomNumGen.nextInt(STANDARD_DECK_SIZE);
+
+                if (completedIndexes[randomIndex] == randomIndex) {
+
+                    int tempRandomIndex;
+                    int tempCount = 1;
+
+                    do { 
+                        tempRandomIndex = randomNumGen.nextInt(STANDARD_DECK_SIZE);
+
+                        if (randomIndex < 51) {
+                            randomIndex++;
+                        } else {
+                            randomIndex = 0;
+                        }
+
+                        tempCount++;
+
+                    } while (completedIndexes[randomIndex] == randomIndex && tempCount < 52);
+
+                    if (tempCount >= 52) {
+                        randomIndex = -1;
+                    } else {
+                        randomIndex = tempRandomIndex;
+                    }
+
+                }
+
+                if (randomIndex > -1) {
+
+                    completedIndexes[randomIndex] = randomIndex;
+                    cards[randomIndex]  = tempCards[i];
+
+                }
+
+            }
+
+        }
+    
     }
 
-    public String toString() {
+    public void toStringDeck() {
 
-
+        
 
     }
 }
