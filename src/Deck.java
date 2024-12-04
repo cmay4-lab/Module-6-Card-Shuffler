410085d42300ecf533d3b2a4ed9adceec1790ebc
cmay4-lab <cmay4@bucs.fsw.edu>
2024-12-03 21:11:29 -0500

    Fixed for real

---
+++ src/Deck.java
@@ -0,0 +1,28 @@
+public class Deck {
+    private Card[] cards;   // Array of cards
+
+    /**
+     * Creates empty deck.
+     * 
+     */
+    public Deck() {
+        cards = new Card[52];
+    }
+
+    public void fill() {
+
+
+    }
+
+    public void shuffle() {
+
+
+
+    }
+
+    public String toString() {
+
+
+
+    }
+}
